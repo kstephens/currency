bb5035a79512ce5ce41d05a6fba59967ee799eec
kstephens <kstephens@a7b57cba-ab17-0410-a052-d51da45ae52a>
2006-10-31 08:51:08 +0000

    New Rakefile.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/currency/currency/trunk@16 a7b57cba-ab17-0410-a052-d51da45ae52a

--- lib/currency.rb
+++ lib/currency.rb
@@ -1,3 +1,4 @@
+# -*- ruby -*-
 
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
@@ -9,4 +10,60 @@ require 'currency/currency'
 require 'currency/money'
 require 'currency/exchange'
 require 'currency/core_extensions'
+
+# The Currency package provides an object-oriented model of:
+#
+# * currencies 
+# * exchanges
+# * exchange rates
+# * monetary values
+#
+# The core classes are:
+#
+# * Currency::Money - uses a scaled integer representation of a monetary value and performs accurate conversions to and from string values.
+# * Currency::Currency - provides an object-oriented representation of a currency.
+# * Currency::Exchange::Base - the base class for a "currency exchange rate" provider.
+# * Currency::Exchange::Rate - represents a exchange rate between two currencies.
+#
+# Below is a basic example:
+#
+#    require 'currency'
+#    require 'currency/exchange/xe'
+#    
+#    usd = Currency::Money('6.78', :USD)
+#    puts "usd = #{usd.format}"
+#    cad = usd.convert(:CAD)
+#    puts "cad = #{cad.format}"
+#
+# The example above uses Currency::Exchange::Xe automatically get exchange rates from http://xe.com/.
+#
+# == ActiveRecord Suppport
+#
+# This package also contains ActiveRecord support for money values:
+#
+#    require 'currency'
+#    require 'currency/active_record'
+#    
+#    class Entry < ActiveRecord::Base
+#       money :amount
+#    end
+# 
+# In the example above, the entries.amount database column is an INTEGER that represents US cents. 
+# The currency code of the money value can be stored in an additional database column or a default currency can be used.
+#
+# == Future Enhancements
+#
+# * storage and retrival of historical exchange rates
+# * exchange rate polling for population of historical rates
+# * support for inflationary rates within a currency, e.g. $10 USD in the year 1955 in 2006 USD.
+# 
+# == SVN Repo
+#
+#    svn checkout svn://rubyforge.org/var/svn/currency/currency/trunk
+#
+# == Examples
+#
+# * The "test programs":http://rubyforge.org/cgi-bin/viewvc.cgi/currency/trunk/examples?root=currency
+# * The "test cases":http://rubyforge.org/cgi-bin/viewvc.cgi/currency/trunk/test/?root=currency
+#
 
