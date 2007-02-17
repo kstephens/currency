bfa97b7822e4825da724ee33a61130307cf6a32f
kstephens <kstephens@a7b57cba-ab17-0410-a052-d51da45ae52a>
2007-02-17 23:55:05 +0000

    Fixed examples.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/currency/currency/trunk@73 a7b57cba-ab17-0410-a052-d51da45ae52a

--- examples/xe1.rb
+++ examples/xe1.rb
@@ -1,9 +1,10 @@
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'currency'
-require 'currency/exchange/xe'
+require 'currency/exchange/rate/source/xe'
 
-ex = Currency::Exchange::Xe.new()
+ex = Currency::Exchange::Rate::Source::Xe.new()
+Currency::Exchange::Rate::Source.current = ex
 
 puts ex.inspect
 puts ex.parse_page_rates.inspect
