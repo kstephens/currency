b1935db563d055fd8e7d99718e78bee11491bf8f
kstephens <kstephens@a7b57cba-ab17-0410-a052-d51da45ae52a>
2007-05-29 19:03:59 +0000

    Use Currency.Money() not Currency::Money.new().
    git-svn-id: svn+ssh://rubyforge.org/var/svn/currency/currency/trunk@114 a7b57cba-ab17-0410-a052-d51da45ae52a

--- examples/ex1.rb
+++ examples/ex1.rb
@@ -3,7 +3,7 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'currency'
 require 'currency/exchange/rate/source/test'
 
-x = Currency::Money.new("1,203.43", :USD)
+x = Currency.Money("1,203.43", :USD)
 
 puts x.to_s
 puts (x * 10).to_s
--- examples/xe1.rb
+++ examples/xe1.rb
@@ -9,7 +9,7 @@ Currency::Exchange::Rate::Source.current = ex
 puts ex.inspect
 puts ex.parse_page_rates.inspect
 
-usd = Currency::Money.new("1", 'USD')
+usd = Currency.Money("1", 'USD')
 
 puts "usd = #{usd}"
 
